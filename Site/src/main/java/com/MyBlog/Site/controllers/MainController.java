4cc0ad56dc2f810c220c2aa230250c399a6d0e5e
postmortrem <101640875+postmortrem@users.noreply.github.com>
2022-11-23 12:14:32 +0400

    Add files via upload

---
+++ Site/src/main/java/com/MyBlog/Site/controllers/MainController.java
@@ -0,0 +1,23 @@
+package com.MyBlog.Site.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/") //при переходе на главную страницу будет вызываться метод снизу
+    public String home(Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "home"; //хоум это название шаблона
+    }
+
+    @GetMapping("/about")
+    public String about(Model model){
+        model.addAttribute("title", "Инфо");
+        return "about";
+    }
+
+}
